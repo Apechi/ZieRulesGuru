eacabe16e4fc2af70e675073460fd6fe1ec81ca3
Raihan Arpe <raihanarpe@gmail.com>
2024-02-02 23:26:27 +0700

    fix: UI

--- app/src/main/java/com/example/zierulesguru/MyApplication.kt
+++ app/src/main/java/com/example/zierulesguru/MyApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 
 open class MyApplication : Application() {
     companion object{
-        val BASE_URL = "https://1510-120-89-90-36.ngrok-free.app/api"
-        val URL = "https://1510-120-89-90-36.ngrok-free.app/"
+        val BASE_URL = "https://b0da-180-244-132-80.ngrok-free.app/api"
+        val URL = "https://b0da-180-244-132-80.ngrok-free.app/"
     }
 }
